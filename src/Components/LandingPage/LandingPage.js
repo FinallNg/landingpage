be46b64ea5e6146b8040eb77725059bbf8d73b58
fahadsarki <fahads.mukhtar@gmail.com>
2022-11-16 09:07:06 +0100

    updates to dependancies, landingpage and services

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -5,11 +5,13 @@ import NewsLetter from './NewsLetter/NewsLetter'
 import FinancialServices from "../LandingPage/FinancialServices/FinancialServices"
 import FeatureCover from './FeatureCover/FeatureCover'
 import Overview from './Overview/Overview'
+import Services from "../LandingPage/Services/Services"
 
 function LandingPage() {
   return (
     <div>
         <HeroSection />
+        <Services />
         <SpendingHabit />
         <FinancialServices />
         <Overview />
